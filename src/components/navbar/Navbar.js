4fab8322b44d4e75d58e6653b8799f62a5535d6e
fazil rahman <fazil2915@gmail.com>
2023-05-29 22:03:16 +0530

    update: footer added

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -11,8 +11,8 @@ export default function Navbar() {
   return (
     <div className="gpt3__navbar">
       <div className="gpt3__navbar-links">
-        <div className="gpt3__navbar-links_logo">
-          <img src={logo} alt='logo'/>
+        <div className="gpt3__navbar-links_logo" >
+          <img src={logo} alt='logo' href='#home'/>
         </div>
         <div className="gpt3__navbar-links_container">
           <p><a href="#home">Home</a></p>
